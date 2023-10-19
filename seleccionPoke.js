37ca7d8c68c7f2a725be4b99593980aaa21e8021
Ttbias2 <tobiasvalentini73@gmail.com>
2023-10-19 15:41:08 -0300

    filtrado y busqueda v1

--- seleccionPoke.js
+++ seleccionPoke.js
@@ -1,21 +1,31 @@
 let olMostrarpokes = document.getElementById("olMostrarpokes");
 let bAvanzar = document.getElementById("bAvanzar");
 let bRetroceder = document.getElementById("bRetroceder");
+let iBuscador = document.getElementById("iBuscador");
+let bBuscar = document.getElementById("bBuscar");
+let bTipo = document.querySelectorAll(".bTipo")
 
 document.addEventListener("DOMContentLoaded",cargarpokes); //funcion que se activa al cargar la pagina y trae a los primero 20 pokes
-bAvanzar.addEventListener("click", masPokes)
-bRetroceder.addEventListener("click", menosPokes)
+bAvanzar.addEventListener("click", masPokes);
+bRetroceder.addEventListener("click", menosPokes);
+iBuscador.addEventListener("keyup", enter);
+bBuscar.addEventListener("click",buscarPorNombID);
 
 let limite = 20;//de cual a cual pokemon va amostrar
 let inicio = 1;
 
 function cargarpokes() {
+
+    bTipo.forEach((boton)=>{
+        boton.addEventListener("click",Tipos);
+    })
+
     var pokemonsArray = [];//array para guardar los poke de las promesas y dps ordenarlos
     var promesas = [];// array para guardar promesas de los fetch
 
     for(let i=inicio;i<=limite;i++)
     {
-        promesas.push(obtenerPokemon(i)); //va a agregar la promesa del fetch de cada pokemon al array promesas
+        promesas.push(obtenerPokemonNombID(i)); //va a agregar la promesa del fetch de cada pokemon al array promesas
     }
 
     Promise.all(promesas) // cuando todas las promesas del array se cumplan ordena los resultados y los carga ordenados en pokemonarry ese array es enviado para mostrar
@@ -25,8 +35,8 @@ function cargarpokes() {
         });
 }
 
-function obtenerPokemon(id) {
-    return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+function obtenerPokemonNombID(i) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
         .then(response => {
             if (response.status == 200) {
                 return response.json();                            //se hace la llamada a la api para traer un pokemon y retorna la promesa de esta llamada
@@ -41,23 +51,35 @@ function obtenerPokemon(id) {
 
 function cargarLista(datos) {
     
+    vaciarLista();
+ 
+    datos.forEach(dato => { // manda todos los datos para que se muestren
+        
+        dibujarApi(dato);
+        
+    });
+}
+
+function vaciarLista()
+{
     while(olMostrarpokes.firstChild)
     {
         olMostrarpokes.removeChild(olMostrarpokes.firstChild);//si tiene algun hijo lo va a borrar
     }
+}
 
-    datos.forEach(dato => {
-        const itemLista = document.createElement("li");
-        const imgPoke = document.createElement("img");
-        const nombrePoke = document.createElement("p"); 
-        imgPoke.src = dato.sprites.front_default;
-        nombrePoke.textContent = dato.name;// se carga el item de la lista             
+function dibujarApi(dato){
 
-        itemLista.appendChild(imgPoke);
-        itemLista.appendChild(nombrePoke); 
+    const itemLista = document.createElement("li");
+    const imgPoke = document.createElement("img");
+    const nombrePoke = document.createElement("p"); 
+    imgPoke.src = dato.sprites.front_default;
+    nombrePoke.textContent = dato.name;// se carga el item de la lista             
 
-        olMostrarpokes.appendChild(itemLista);//se añade el item a la lista
-    });
+    itemLista.appendChild(imgPoke);
+    itemLista.appendChild(nombrePoke); 
+
+    olMostrarpokes.appendChild(itemLista);//se añade el item a la lista
 }
 
 function ordenarPorId(pokemonArray) {
@@ -102,4 +124,80 @@ function menosPokes(){
     }
 
     cargarpokes();
+}
+
+function enter(event){
+// comprueba siempre que se apriete un boton
+//si es enter va a buscar el pokemon
+    if (event.key == "Enter")
+    {
+        event.preventDefault(); 
+        buscarPorNombID();
+    }
+//si el texto vuelve a estar en blanco se mostraran lso ultimos 20 y se podra avanzar y retroceder de nuevo
+    if(iBuscador.value == "")
+    {
+        bAvanzar.disabled = false;
+        bRetroceder.disabled = false; 
+        cargarpokes();
+    }
+
+}
+
+async function buscarPorNombID()
+{
+    let pokemon = await obtenerPokemonNombID(iBuscador.value);
+
+    if(pokemon.id <= 151)//si el pokemon es de la primera generacion se mostrara y se desabilitara los botones para cambiar de pagina
+    {
+        vaciarLista();
+        await dibujarApi(pokemon);
+        bAvanzar.disabled = true;
+        bRetroceder.disabled = true;    
+    }
+    
+}
+
+function Tipos()
+{
+   fetch(`https://pokeapi.co/api/v2/type/${this.value}/`)
+   .then(response => {
+        if(response.status == 200)
+        {
+            return response.json();
+        }
+        else
+        {
+            throw new Error("Error en la repuesta de la API");
+        }
+   })
+   .then(data => {
+    let pokemons = filtrarPrimeraGenTipo(data);
+   })
+   .catch(error => {
+     console.error("ha ocurrido un error",error);
+   })
+}
+
+function filtrarPrimeraGenTipo(data){
+
+    let listapoke = [];
+    let promesas = []; 
+
+    data.pokemon.forEach(pokemon => {   
+        promesas.push(obtenerPokemonNombID(pokemon.pokemon.name));     
+    });
+
+    Promise.all(promesas)
+    .then(resultados => {
+        resultados.forEach(pokemon=>{
+            if(pokemon.id < 152)
+            {
+               listapoke.push(pokemon); 
+            }
+        })
+
+        cargarLista(listapoke);
+    })
+
 }
