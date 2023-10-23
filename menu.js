0f67d1d9b4ab5c945e37c973461a75a99e94d550
Franco Bernardele <francobernar@gmail.com>
2023-10-23 11:44:44 -0300

    html y js corregido

---
+++ menu.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nombre1 = document.getElementById('name1');
+    const nombre2 = document.getElementById('name2');
+    const btn = document.getElementById('btn');
+
+    console.log('hola');
+    btn.onclick = function(){
+        console.log('nombre: ', nombre1.value);
+        console.log('nombre: ', nombre2.value);
+    }
+    
+    function getNombre()
+    {
+        if(nombre1.value === ''||nombre2.value==='')
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+});
