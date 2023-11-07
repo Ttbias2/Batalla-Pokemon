438da237a28b8156555bb636b1b65fac82ac262e
Franco Bernardele <67322352+Franspi-lol@users.noreply.github.com>
2023-11-06 23:00:38 -0300

    moves folder

---
+++ src/app/moves/moves.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Move } from './moves.model';
+@Component({
+  selector: 'app-moves',
+  templateUrl: './moves.component.html',
+  styleUrls: ['./moves.component.css']
+})
+export class MovesComponent {
+
+
+  
+}
