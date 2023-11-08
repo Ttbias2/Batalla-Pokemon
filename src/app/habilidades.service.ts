bf68231a6c3b74c2ce854dfbca3c1ce1b024a367
Franco Bernardele <francobernar@gmail.com>
2023-11-07 22:04:09 -0300

    habilidades

---
+++ src/app/habilidades.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { habilidad } from './Clases/habilidad.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HabilidadesService {
+
+  constructor() { }
+}
